214eaa502ccfc1fcc32ba902c407b2e68a93a877
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-12 17:55:23 +0800

    add quizProfile and attemptedQuestion

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, QuizProfile, AttemptedQuestion
 from .forms import QuestionForm, ChoiceForm, ChoiceInlineFormset
 # Register your models here.
 
@@ -33,3 +33,5 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(QuizProfile)
+admin.site.register(AttemptedQuestion)
